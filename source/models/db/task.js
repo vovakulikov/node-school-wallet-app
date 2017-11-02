af62c8908877b696eb7508439d3e7ad58122529e
valentingordienko <danadj@yandex.ru>
2017-11-02 21:48:58 +0300

    DEV: Реализована модель данных для задачи;

---
+++ source/models/db/task.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const Task = mongoose.model('Task', {
+    id: {
+        type: Number,
+        required: true
+    },
+    label: {
+        type: String,
+        required: true
+    },
+    from: {
+        type: Number,
+        required: true
+    },
+    target: {
+        type: {
+            type: String,
+            required: true
+        },
+        number: {
+            type: Number,
+            required: true
+        }
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    period: {
+        type: {
+            type: String,
+            required: true
+        },
+        value: {
+            type: String,
+            required: true
+        }
+    },
+    createDate: {
+        type: String
+    },
+    lastExecution: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = Task;
