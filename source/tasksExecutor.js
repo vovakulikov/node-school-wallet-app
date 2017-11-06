2be0a941702f4dfd33dd5610c7321b8798e04cbc
yetihead <yetibrain@yandex.ru>
2017-11-06 08:39:24 +0300

    вынес определение обработчиков, обявление и инициализацию моделей и подключение за пределы scheduleJob. Поправил формирование поля data в транзакции.

--- source/tasksExecutor.js
+++ source/tasksExecutor.js
@@ -1,102 +1,111 @@
-var crontab = require('node-crontab');
-var jobId = crontab.scheduleJob("*/5 * * * *", function(){
+const crontab = require('node-crontab');
+const asnc = require('async');
+const logger = require('libs/logger')('task-executor');
+const TaskModel = require('./models/tasks');
+const CardModel = require('./models/cards');
+const TransactionsModel = require('./models/transactions');
 
-	//This will call this function every 1 minute
-	//console.log("It's been 1 minute!");
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/school-wallet', {useMongoClient: true});
+mongoose.Promise = global.Promise;
 
-	const asnc = require('async');
-	const logger = require('libs/logger')('task-executor');
-	const TaskModel = require('./models/tasks');
-	const CardModel = require('./models/cards');
-	const TransactionsModel = require('./models/transactions');
+const mTasks = new TaskModel();
+const mCards = new CardModel();
+const mTrans = new TransactionsModel();
 
-	const mongoose = require('mongoose');
-	mongoose.connect('mongodb://localhost/school-wallet', { useMongoClient: true });
-	mongoose.Promise = global.Promise;
+crontab.scheduleJob('*/5 * * * *', () => {
+	// This will call this function every 1 minute
+	// console.log("It's been 1 minute!");
 
-	const mTasks = new TaskModel();
-	const mCards = new CardModel();
-	const mTrans = new TransactionsModel();
-
-	logger.info('tasks executor started');
+	logger.info('Tasks executor started.');
 
 	mTasks.executionList()
 		.then(taskListHandler)
 		.catch(closeConnection);
-
-
-	function taskListHandler (list) {
-		// если нечего выполнять, завершаем работу
-		if (!list || !list.length) {
-			return closeConnection();
-		}
-
-		// идем по списку задач и выполняем их
-		asnc.each(list, (task, callback) => {
-			executeTask(task)
-				.then(_ => callback()) // задача выполнилась
-				.catch(err => callback(err)); // задача не смогла выполниться
-		// в конце закрываем соединение
-		}, closeConnection);
-	}
-
-	function closeConnection (err) {
-		mongoose.connection.close();
-		if (err) {
-			logger.error('Task executor done with error', err);
-			return;
-		}
-		logger.info('Task executor done.');
-	}
-
-	async function executeTask({id, from, amount, target}) {
-		let data = target.number, resultTransaction, targetCard, card = await mCards.get(from);
-
-		if (!card) {
-			// если нет карты
-			throw new Error(`Card id=${from} not found`);
-		}
-
-		if (amount > card.balance) {
-			// недостаточно средств
-			// что делать когда недостаточно средств, считать невыполненной?
-			logger.warn(`Balance is not enough for task on cards id = ${from}`);
-			return;
-		}
-
-		// если задача = перевод на карту, пополняем её
-		if (target.type === 'card2Card') {
-			const num = parseInt(target.number, 10);
-			targetCard = await mCards.get(num);
-			if (!targetCard) {
-				// нет карты, принимающей средства
-				return;
-			}
-
-			data = targetCard.cardNumber;
-			// начисляем и создаем транзакцию
-			await mCards.refill(num, amount);
-			await mTrans.create({
-				cardId: num,
-				type: target.type,
-				data: card.cardNumber,
-				sum: amount.toString(10)
-			});
-		}
-
-		// списываем с карты сумму
-		card = await mCards.withdraw(from, amount);
-
-		// создаем транзакцию
-		return mTrans.create({
-			cardId: from,
-			type: target.type,
-			data: data,
-			sum: '-' + amount
-		}).then( _ => {
-			// помечаем задачу выполненной
-			return mTasks.done(id).catch(err=>logger.error('Error while marking task done ',err));
-		});
-	}
-
 });
+
+function taskListHandler(list) {
+	// если нечего выполнять, завершаем работу
+	if (!list || !list.length) {
+		return closeConnection();
+	}
+
+	// идем по списку задач и выполняем их
+	asnc.each(list, (task, callback) => {
+		executeTask(task)
+			.then(_ => callback()) // задача выполнилась
+			.catch(err => callback(err)); // задача не смогла выполниться
+	// в конце закрываем соединение
+	}, closeConnection);
+}
+
+function closeConnection(err) {
+	// mongoose.connection.close();
+	if (err) {
+		logger.error('Tasks executor done with error', err);
+		return;
+	}
+	logger.info('Tasks executor done.');
+}
+
+async function executeTask({id, from, amount, target}) {
+	let card = await mCards.get(from);
+	let data = null;
+
+	if (!card) {
+		// если нет карты
+		throw new Error(`Card id=${from} not found`);
+	}
+
+	if (amount > card.balance) {
+		// недостаточно средств
+		// что делать когда недостаточно средств, считать невыполненной?
+		logger.warn(`Balance is not enough for task on cards id = ${from}`);
+		return;
+	}
+
+	switch (target.type) {
+		case 'card2Card':
+			data = await card2CardPay(parseInt(target.number, 10), amount, card, id);
+			break;
+		case 'paymentMobile':
+			data = {phoneNumber: target.number};
+			break;
+		default:
+			throw new Error(`Unexpected payment type in task id=${id}`);
+	}
+
+	// списываем с карты сумму
+	card = await mCards.withdraw(from, amount);
+
+	// создаем транзакцию
+	return mTrans.create({
+		cardId: from,
+		type: target.type,
+		sum: '-' + amount,
+		data,
+	}).then( _ => {
+		// помечаем задачу выполненной
+		return mTasks.done(id).catch(err=>logger.error('Error while marking task done ',err));
+	});
+}
+
+async function card2CardPay (cardId, amount, sourceCard, id) {
+	const targetCard = await mCards.get(cardId);
+	if (!targetCard) {
+		// нет карты, принимающей средства
+		throw new Error(`Target card id=${cardId} not found for task id=${id}`);
+	}
+	// начисляем и создаем транзакцию
+	await mCards.refill(cardId, amount);
+	await mTrans.create({
+		cardId,
+		type: 'card2Card',
+		data: {cardNumber: sourceCard.cardNumber},
+		sum: amount.toString(10)
+	});
+
+	return {
+		cardNumber: targetCard.cardNumber
+	};
+}
